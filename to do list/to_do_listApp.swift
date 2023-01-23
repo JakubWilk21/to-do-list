c34e078d111dda0b5ec56667e1c87e8aa3c45f40
Jakub <Wilk>
2023-01-23 09:29:58 -0600

    Initial Commit

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Jakub Wilk on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
