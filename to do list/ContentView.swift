d863229c72523b4e994e52f83b246729b2bd5bd9
Jakub <Wilk>
2023-01-23 19:48:40 -0600

    Created initial To-Do list

--- to do list/ContentView.swift
+++ to do list/ContentView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var toDoItems =
-            [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-             ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-             ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+     ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+     ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     var body: some View {
         NavigationView {
             List {
                 ForEach(toDoItems) { item in
-                    Text(item.description)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                    }
+                    .onMove { indices, newOffset in
+                        toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    }
+                    .onDelete { indexSet in
+                        toDoItems.remove(atOffsets: indexSet)
+                    }
                 }
-                .onMove { indices, newOffset in
-                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
-                }
-                .onDelete { indexSet in
-                    toDoItems.remove(atOffsets: indexSet)
-                }
-            }
                 .navigationBarTitle("To Do List", displayMode: .inline)
                 .navigationBarItems(leading: EditButton())
             }
@@ -36,11 +44,11 @@ struct ContentView: View {
             ContentView()
         }
     }
-
-struct ToDoItem: Identifiable {
-    var id = UUID()
-    var priority = String()
-    var description = String()
-    var dueDate = Date()
-}
-
+    
+    struct ToDoItem: Identifiable {
+        var id = UUID()
+        var priority = String()
+        var description = String()
+        var dueDate = Date()
+    }
+    
